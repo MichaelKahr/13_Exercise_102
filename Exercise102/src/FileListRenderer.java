a7b3b36402b8b161df23cebd898fe831d6b2184d
Michael Kahr <mikey.kahr@gmail.com>
2018-09-25 09:37:51 +0200

    something

--- Exercise102/src/FileListRenderer.java
+++ Exercise102/src/FileListRenderer.java
@@ -1,3 +1,12 @@
-public class FileListRenderer {
+
+import java.awt.Component;
+import javax.swing.JList;
+import javax.swing.ListCellRenderer;
+
+public class FileListRenderer implements ListCellRenderer {
+
+    @Override
+    public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+    }
 
 }
---
+++ Exercise102/src/FileModell.java
@@ -0,0 +1,14 @@
+
+import javax.swing.AbstractListModel;
+
+public class FileModell extends AbstractListModel{
+
+    @Override
+    public int getSize() {
+    }
+
+    @Override
+    public Object getElementAt(int index) {
+    }
+
+}
